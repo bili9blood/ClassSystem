a91d75249c23f9e3f6e63cbeed956c515580dd12
bili9blood <3501202786@qq.com>
2023-08-06 07:23:13 +0800

    🐞 fix(main.cpp -> switchToDesktop()): Deleted

--- src/ClassSystem/main.cpp
+++ src/ClassSystem/main.cpp
@@ -5,25 +5,6 @@
 #include "Widgets/MainPanel.h"
 #include "Widgets/PopupMenu.h"
 
-/**
- * @brief do the same thing as press `<Win> + D`
- *
- */
-void switchToDesktop() {
-  INPUT input[4];
-  memset(input, 0, sizeof(input));
-
-  input[0].type = input[1].type = input[2].type = input[3].type =
-      INPUT_KEYBOARD;
-
-  input[0].ki.wVk = input[3].ki.wVk = VK_LWIN;
-  input[1].ki.wVk = input[2].ki.wVk = 'D';
-
-  input[2].ki.dwFlags = input[3].ki.dwFlags = KEYEVENTF_KEYUP;
-
-  SendInput(4, input, sizeof(INPUT));
-}
-
 int main(int argc, char *argv[]) {
   // single application
   QSharedMemory sm("CLASS-SYSTEM-SINGLE-KEY");
@@ -46,6 +27,5 @@ int main(int argc, char *argv[]) {
   // generate template .stm file
   // ClassData::writeTo(ClassData::testData(), new QFile("data.stm"));
 
-  switchToDesktop();
   return QApplication::exec();
 }
