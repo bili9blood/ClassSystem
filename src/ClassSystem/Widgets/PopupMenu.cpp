686e0a4519d06e2b20a8e6f249b9a287f4586dbd
bili9blood <3501202786@qq.com>
2023-08-06 08:10:47 +0800

    ✨ feat(PopupMenu): 可以在屏幕右边缘移动

--- src/ClassSystem/Widgets/PopupMenu.cpp
+++ src/ClassSystem/Widgets/PopupMenu.cpp
@@ -10,14 +10,19 @@ PopupMenu::PopupMenu(QWidget *parent)
   m_popMenuLabel->setPixmap(QPixmap(":/img/menu.png"));
   // init layouts
   m_mainLayout->addWidget(m_popMenuLabel);
+  m_mainLayout->addLayout(m_btnLayout);
+
   m_mainLayout->setMargin(5);
 }
 
 void PopupMenu::paintEvent(QPaintEvent *) {
-  move(QApplication::primaryScreen()->size().width() - width(), y());
+  if (m_init) {
+    move(QApplication::primaryScreen()->size().width() - width(), y());
+    m_init = false;
+  }
 
   QPainter painter(this);
-  painter.setBrush(QColor(31, 33, 34, 235));
+  painter.setBrush(QColor(31, 33, 34, 185));
   painter.setPen(Qt::transparent);
   painter.drawRoundedRect(QRect(rect().topLeft(), rect().size() + QSize(10, 0)),
                           10, 10);
@@ -25,4 +30,10 @@ void PopupMenu::paintEvent(QPaintEvent *) {
 
 bool PopupMenu::eventFilter(QObject *obj, QEvent *ev) { return false; }
 
-void PopupMenu::onMenuClicked(size_t menuID) {}
+void PopupMenu::mousePressEvent(QMouseEvent *ev) {
+  m_mouseStartYPos = frameGeometry().y() - ev->globalY();
+}
+void PopupMenu::mouseMoveEvent(QMouseEvent *ev) {
+  if (ev->buttons() & Qt::LeftButton)
+    move(x(), ev->globalY() + m_mouseStartYPos);
+}
--- src/ClassSystem/Widgets/PopupMenu.h
+++ src/ClassSystem/Widgets/PopupMenu.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <qapplication.h>
 #include <qboxlayout.h>
-#include <qdesktopwidget.h>
+#include <qevent.h>
 #include <qlabel.h>
 #include <qscreen.h>
 
@@ -11,14 +11,16 @@ class PopupMenu : public QWidget {
   explicit PopupMenu(QWidget *parent = nullptr);
 
  private:
-  QDesktopWidget m_desktop;
   QLabel *m_popMenuLabel = new QLabel(this);
   QHBoxLayout *m_mainLayout = new QHBoxLayout(this);
-  QPoint m_startPoint;
- private slots:
-  void onMenuClicked(size_t menuID);
+  QHBoxLayout *m_btnLayout = new QHBoxLayout();
+
+  int m_mouseStartYPos;
+  bool m_init = true;
 
  protected:
   bool eventFilter(QObject *obj, QEvent *ev) override;
   void paintEvent(QPaintEvent *ev) override;
+  void mousePressEvent(QMouseEvent *ev) override;
+  void mouseMoveEvent(QMouseEvent *ev) override;
 };
