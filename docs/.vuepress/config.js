90b6e222e88b9175f7b3badbf5ae7703634ee14e
bili9blood <3501202786@qq.com>
2023-08-22 14:25:37 +0800

    docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -50,4 +50,32 @@ module.exports = {
       },
     ],
   ],
+
+  configureWebPack: () => {
+    const NODE_ENV = process.env.NODE_ENV;
+    //判断是否是生产环境
+    if (NODE_ENV === "production") {
+      return {
+        output: {
+          publicPath:
+            "https://cdn.jsdelivr.net/gh/bili9Blood/class-system-docs/",
+        },
+        resolve: {
+          //配置路径别名
+          alias: {
+            public: path.resolve(__dirname, "./public"),
+          },
+        },
+      };
+    } else {
+      return {
+        resolve: {
+          //配置路径别名
+          alias: {
+            public: path.resolve(__dirname, "./public"),
+          },
+        },
+      };
+    }
+  },
 };
