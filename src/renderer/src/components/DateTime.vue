b768d7d6ff53945551a3002e72596a14e37965ef
bili9blood <3501202786@qq.com>
2023-11-07 21:27:09 +0800

    时间显示增加阴影，做好显示日期的准备

--- src/renderer/src/components/DateTime.vue
+++ src/renderer/src/components/DateTime.vue
@@ -28,23 +28,52 @@ setInterval(() => {
 }, 500);
 </script>
 <template>
-  <div class="time">
-    <span class="text">{{ hoursRef }}</span>
-    <pre class="colons">{{ colonsRef }}</pre>
-    <span class="text">{{ minsRef }}</span>
-    <pre class="colons">{{ colonsRef }}</pre>
-    <span class="text">{{ secsRef }}</span>
+  <div class="wrapper">
+    <div class="time">
+      <span>{{ hoursRef }}</span>
+      <pre>{{ colonsRef }}</pre>
+      <span>{{ minsRef }}</span>
+      <pre>{{ colonsRef }}</pre>
+      <span>{{ secsRef }}</span>
+    </div>
+
+    <div class="day"></div>
+    <div class="date"></div>
   </div>
 </template>
 <style scoped lang="less">
-.time {
-  font-size: 2rem;
+.wrapper {
+  display: grid;
+
+  grid-gap: 20px;
+  grid-template-columns: repeat(2, auto);
+  grid-template-rows: repeat(2, auto);
+
+  align-items: center;
+  justify-items: center;
 }
-.colons {
-  display: inline;
-  font-family: 华文中宋;
-  width: 2em;
-  position: relative;
-  bottom: 0.3rem;
+
+.time {
+  grid-column: 1;
+  grid-row: 1 / span 2;
+  font-size: 2.5rem;
+  text-shadow: #00000066 1px 1px 4px;
+
+  pre {
+    display: inline;
+    font-family: 华文中宋;
+    width: 2em;
+    position: relative;
+    bottom: 0.3rem;
+  }
+}
+
+.day {
+  grid-row: 1;
+}
+
+.date {
+  grid-row: 2;
+  grid-column: 2;
 }
 </style>
