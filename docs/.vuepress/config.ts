30115c5e17c103250e7edb9c38890d3508f68f34
bili9blood <3501202786@qq.com>
2023-11-23 19:23:52 +0800

    docs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,11 +4,13 @@ import theme from "./theme";
 export default defineUserConfig({
   title: "ClassSystem 使用文档",
   lang: "zh-CN",
-  
+
   head: [
     ["link", { rel: "icon", href: "/img/logo.ico" }],
     ["link", { rel: "icon", href: "/img/logo.png" }],
   ],
+
+  base: "/docs/",
 
   theme,
 
