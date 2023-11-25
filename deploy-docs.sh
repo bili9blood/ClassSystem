e025cbaee09556a0ed2691873e0bf6ab915a4468
bili9blood <3501202786@qq.com>
2023-11-25 19:05:26 +0800

    workflow

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -17,16 +17,12 @@ yarn global add vuepress
 NODE_OPTIONS=--openssl-legacy-provider
 yarn run docs:build
 
-# 进入生成的文件夹
 yes | cp -rf .vuepress/dist/. publish
 
 cd publish
 
 git config user.name "class-system"
 git config user.email "3501202786@qq.com"
-
-unset http_proxy
-unset https_proxy
 
 git add .
 git commit -m 'deploy'
