7c62542a8b97a608c4bb58555fd8a765e6bb9973
bili9blood <3501202786@qq.com>
2023-11-25 15:13:18 +0800

    workflow

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -9,15 +9,8 @@ cd docs
 
 GITEE_REPO=https://class-system:$1@gitee.com/class-system/docs.git
 
-mkdir .vuepress/dist
-git clone $GITEE_REPO .vuepress/dist
-
-cd .vuepress/dist
-
-echo "status before"
-git status
-
-cd -
+mkdir publish
+git clone $GITEE_REPO publish
 
 # 生成静态文件
 yarn install
@@ -26,20 +19,18 @@ NODE_OPTIONS=--openssl-legacy-provider
 yarn run docs:build
 
 # 进入生成的文件夹
-cd .vuepress/dist
+yes | cp -rf .vuepress/dist/. publish
 
-echo "status after"
-echo $PWD
-git status
+cd publish
 
-# git config user.name "class-system"
-# git config user.email "3501202786@qq.com"
+git config user.name "class-system"
+git config user.email "3501202786@qq.com"
 
-# unset http_proxy
-# unset https_proxy
+unset http_proxy
+unset https_proxy
 
-# git add .
-# git commit -m 'deploy'
+git add .
+git commit -m 'deploy'
 
-# git push $GITEE_REPO master
-# echo "Done."
+git push origin master
+echo "Done."
